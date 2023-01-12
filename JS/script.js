71244a6a9d04543e63414a0abfc54854f1ce6655
Muxop <122389790+Muxop@users.noreply.github.com>
2023-01-12 22:04:24 +0100

    Push-1

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+// Vive Monsieur Hachem 
+// Cette partie sert pour l'animation de transition du menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
